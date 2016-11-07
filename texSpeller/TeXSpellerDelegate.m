c8182d6d1e7d363c22f028be83546f0372745980
Yuji Tachikawa <yuji.tachikawa@ipmu.jp>
2016-11-07 22:40:03 +0900

    Implementing naive forwarding.

--- texSpeller/TeXSpellerDelegate.m
+++ texSpeller/TeXSpellerDelegate.m
@@ -21,77 +21,24 @@
 }
 
 #pragma mark - old api
-#if 0
 - (NSRange)spellServer:(NSSpellServer *)sender findMisspelledWordInString:(NSString *)stringToCheck language:(NSString *)language wordCount:(NSInteger *)wordCount countOnly:(BOOL)countOnly{
-    
-    NSLog(@"Old api - find mispelled word in string: %@", stringToCheck);
-    
-    *wordCount = -1;
-    return [stringToCheck rangeOfString:@"badword"];
+    NSLog(@"%@",stringToCheck);
+    return [checker checkSpellingOfString:stringToCheck startingAt:0 language:language wrap:NO inSpellDocumentWithTag:0 wordCount:wordCount];
 }
 
 - (nullable NSArray<NSString *> *)spellServer:(NSSpellServer *)sender suggestGuessesForWord:(NSString *)word inLanguage:(NSString *)language{
-    
-    NSLog(@"Old api - suggest guesses for word: %@", word);
-    
-    if ([word isEqualToString:@"badword"]) {
-        return @[@"goodword"];
-    }
-    
-    return nil;
+    NSArray<NSString*>* guesses=[checker guessesForWordRange:NSMakeRange(0,[word length]) inString:word language:language inSpellDocumentWithTag:0];
+    return guesses;
 }
 
-
-#endif
-
-#pragma mark - new api
--(NSString *) languageFromOthrography:(NSOrthography *)orthrography
+- (void)spellServer:(NSSpellServer *)sender didForgetWord:(NSString *)word inLanguage:(NSString *)language
 {
-    if(orthrography && orthrography.allLanguages.firstObject) {
-        return orthrography.allLanguages.firstObject;
-    } else {
-        return [[NSLocale currentLocale] valueForKey:NSLocaleLanguageCode];
-    }
+    [checker unlearnWord:word];
+}
+- (void)spellServer:(NSSpellServer *)sender didLearnWord:(nonnull NSString *)word inLanguage:(nonnull NSString *)language
+{
+    [checker learnWord:word];
 }
 
--(nullable NSArray<NSTextCheckingResult *> *)spellServer:(NSSpellServer *)sender checkString:(NSString *)stringToCheck offset:(NSUInteger)offset types:(NSTextCheckingTypes)checkingTypes options:(nullable NSDictionary<NSString *, id> *)options orthography:(nullable NSOrthography *)orthography wordCount:(NSInteger *)wordCount {
-    NSOrthography*o;
-    NSArray<NSTextCheckingResult *> * results=[checker checkString:stringToCheck range:NSMakeRange(offset,[stringToCheck length]-offset) types:checkingTypes options:options inSpellDocumentWithTag:0 orthography:&o wordCount:wordCount];
-    return results;
-#if 0
-    
-    BOOL checkSpelling = (checkingTypes & NSTextCheckingTypeSpelling) > 0;
-    BOOL provideCorrections = (checkingTypes & NSTextCheckingTypeCorrection) > 0;
-    
-    NSString *language = [self languageFromOthrography:orthography];
-    NSLog(@"New api method, language: %@, for %@, offset: %lu, spelling: %d, corrections: %d", language, stringToCheck, (unsigned long)offset, checkSpelling, provideCorrections);
-    
-    *wordCount = -1;
-    NSMutableArray<NSTextCheckingResult *> *results = [NSMutableArray new];
-    
-    NSRange range = NSMakeRange(0, stringToCheck.length);
-    NSStringCompareOptions compareOptions = NSDiacriticInsensitiveSearch | NSCaseInsensitiveSearch;
-    
-    //check all strings
-    while (true) {
-        NSRange foundRange = [stringToCheck rangeOfString:@"badword" options:compareOptions range:range];
-        if (foundRange.location != NSNotFound) {
-            range.location = foundRange.location + foundRange.length;
-            range.length = stringToCheck.length - range.location;
-            
-            NSRange rangeWithOffset = NSMakeRange(foundRange.location + offset, foundRange.length);
-            
-            [results addObject:[NSTextCheckingResult spellCheckingResultWithRange:rangeWithOffset]];
-            
-            //you can remove this line, if you are also providing method 'suggestGuessesForWord:...'
-            [results addObject:[NSTextCheckingResult correctionCheckingResultWithRange:rangeWithOffset replacementString:@"goodword"]];
-        }else {
-            break;
-        }
-    }
-    
-    return results;
-#endif
-}
 
 @end
